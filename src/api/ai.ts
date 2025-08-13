54f53fa614965c5626809eb064a7cbeb1796629c
Bohdan <matula.bohdan@gmail.com>
2025-08-13 16:57:00 +0300

    upd with model

--- src/api/ai.ts
+++ src/api/ai.ts
@@ -1,12 +1,12 @@
 import type { IData } from "../types/custom";
 
-export async function analyzeData(data: IData[], prompt: string): Promise<string> {
+export async function analyzeData(data: IData[], prompt: string, model?: string): Promise<string> {
     prompt = "Проаналізуй промпт та дату та дай конкретну відповідь на запитання на тій мові яка буде далі в промпті (якщо ангд то на англ очікую відповідь). Ще не роби ніяких таблиць чи виділень тексту (жирним чи іншим) чи формул - відповідь абзацами тексту. Дозволяються смайлики, небагато." + prompt;
     try {
         const response = await fetch("https://fintrack-4izg.onrender.com/analyze", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ data, prompt }),
+            body: JSON.stringify({ data, prompt, model }),
         });
 
         if (!response.ok) {
@@ -14,7 +14,7 @@ export async function analyzeData(data: IData[], prompt: string): Promise<string
         }
 
         const json = await response.json();
-        return json.choices?.[0]?.message?.content || "No response content";
+        return json.result || "No response content";;
     } catch (error) {
         console.error("Analyze error:", error);
         throw error;
